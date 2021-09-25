0ada626926ff92e3bd4aea68ebd50d379e310a14
Chris Norman <chris.norman2@googlemail.com>
2021-09-25 12:58:56 +0100

    Set window title instead of using print.

--- example/event_example.dart
+++ example/event_example.dart
@@ -1,17 +1,14 @@
-// ignore_for_file: avoid_print
-
 import 'package:dart_sdl/dart_sdl.dart';
 
 Future<void> main() async {
   final sdl = Sdl()..init();
   final window = sdl.createWindow('Events Example');
   await for (final event in sdl.events) {
-    print(event);
+    window.title = event.toString();
     if (event is QuitEvent) {
       break;
     } else if (event is KeyboardEvent) {
-      print(event.key.keycode);
-      print(event.key.modifiers);
+      window.title = '${event.key.keycode} (${event.key.modifiers}';
       if (event.key.keycode == KeyCode.keycode_q &&
               event.key.modifiers.isEmpty ||
           event.key.keycode == KeyCode.keycode_ESCAPE) {
