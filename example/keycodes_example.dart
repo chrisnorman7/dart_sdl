29309fce63c74dc358a4b290776a79c8b836bcf4
Chris Norman <chris.norman2@googlemail.com>
2021-09-25 13:00:28 +0100

    Add a more in depth keyboard example.
    Fixes #1

---
+++ example/keycodes_example.dart
@@ -0,0 +1,31 @@
+import 'package:dart_sdl/dart_sdl.dart';
+
+/// A more complicated keyboard example.
+Future<void> main() async {
+  final sdl = Sdl()..init();
+  final window = sdl.createWindow('Keycodes Example');
+  final ms = (1000 / 60).round();
+  while (true) {
+    Event? event;
+    do {
+      event = sdl.pollEvent();
+    } while (event != null);
+    final keyState = sdl.keyboardState;
+    if (keyState[ScanCode.SCANCODE_ESCAPE.toSdlValue()] ==
+        PressedState.pressed) {
+      break;
+    } else {
+      for (final scanCode in ScanCode.values) {
+        if (scanCode == ScanCode.NUM_SCANCODES) {
+          continue;
+        }
+        if (keyState[scanCode.toSdlValue()] == PressedState.pressed) {
+          window.title = scanCode.toString();
+        }
+      }
+    }
+    await Future<void>.delayed(Duration(milliseconds: ms));
+  }
+  window.destroy();
+  sdl.quit();
+}
