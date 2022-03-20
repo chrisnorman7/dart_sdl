35a9fbb06dd843e3156a66a061c3d89cebfe0a86
Chris Norman <chris.norman2@googlemail.com>
2022-03-20 22:25:04 +0000

    Don't even bother closing joysticks.

--- example/joystick_example.dart
+++ example/joystick_example.dart
@@ -24,7 +24,6 @@ void main() {
     print('Number of balls: ${j.numBalls}.');
     print('Number of buttons: ${j.numButtons}.');
     print('Number of hats: ${j.numHats}.');
-    j.close();
   }
   sdl.quit();
 }
